9d31c7e590943d0d34342b47241796c7b19f0cae
dbmesquita <dionata.bm@gmail.com>
2022-02-18 16:49:43 -0300

    validações da  questão 2

--- src/Questao_2.java
+++ src/Questao_2.java
@@ -14,21 +14,23 @@ public class Questao_2 {
         int validation3 = 0;
         int validation4 = 0;
         int validation5 = 0;
+        int validation = 0;
 
-        System.out.println("Sua Senha precisar ser forte!:");
-        System.out.println("*Possui no mínimo 6 caracteres.");
-        System.out.println("*Contém no mínimo 1 digito.");
-        System.out.println("*Contém no mínimo 1 letra em minúsculo.");
-        System.out.println("*Contém no mínimo 1 letra em maiúsculo.");
-        System.out.println("*Contém no mínimo 1 caractere especial: !@#$%^&*()-+");
-        System.out.println();
-        System.out.println("Digite sua senha:");
+        System.out.println("\n");
+        System.out.println("Sua Senha precisar ser forte!");
+        System.out.println("Nela deve conter:");
+        System.out.println("*No mínimo 6 caracteres.");
+        System.out.println("*No mínimo 1 digito.");
+        System.out.println("*No mínimo 1 letra em minúsculo.");
+        System.out.println("*No mínimo 1 letra em maiúsculo.");
+        System.out.println("*No mínimo 1 caractere especial: !@#$%^&*()-+");
+        System.out.println("\n");
+        System.out.println("Digite sua senha abaixo:");
 
         while (input.hasNextLine()) {
             passwd = input.nextLine();
 
-
-            if (passwd.length() >= 6 && passwd.length() <= 8) {
+            if (passwd.length() >= 6 && passwd.length() <= 32) {
                 validation1 = 0;
             } else {
                 validation1 = 1;
@@ -53,34 +55,58 @@ public class Questao_2 {
             } else {
                 validation5 = 1;
             }
-            switch (validation1) {
-                case 1:
-                    System.out.println("*Deve possui no mínimo 6 caracteres.");
-                default:
+            if ((validation1 + validation2 + validation3 + validation4 + validation5) == 0) {
+                validation = 0;
+            } else {
+                validation = 1;
             }
-            switch (validation2) {
-                case 1:
-                    System.out.println("*Contém no mínimo 1 digito.");
-                default:
-            }
-            switch (validation3) {
-                case 1:
-                    System.out.println("*Contém no mínimo 1 letra em minúsculo.");
-                default:
-            }
-            switch (validation4) {
-                case 1:
-                    System.out.println("*Contém no mínimo 1 letra em maiúsculo.");
-                default:
-            }
-            switch (validation5) {
-                case 1:
-                    System.out.println("*Contém no mínimo 1 caractere especial: !@#$%^&*()-+");
+
+
+            switch (validation) {
+                case 0:
+                    System.out.println("Senha valida e cadastrada!");
                 default:
             }
 
-            System.out.println("Digite Novamente:");
- break;
+
+            while (validation ==1 ){
+
+                switch (validation) {
+                    case 1:
+                        System.out.println("Hum, faltou algo, veja abaixo o que faltou!");
+                    default:
+                }
+
+                switch (validation1) {
+                    case 1:
+                        System.out.println("*Deve possuir no mínimo 6 caracteres.");
+                    default:
+                }
+                switch (validation2) {
+                    case 1:
+                        System.out.println("*Conter no mínimo 1 digito.");
+                    default:
+                }
+                switch (validation3) {
+                    case 1:
+                        System.out.println("*Contém no mínimo 1 letra em minúsculo.");
+                    default:
+                }
+                switch (validation4) {
+                    case 1:
+                        System.out.println("*Contém no mínimo 1 letra em maiúsculo.");
+                    default:
+                }
+                switch (validation5) {
+                    case 1:
+                        System.out.println("*Contém no mínimo 1 caractere especial: !@#$%^&*()-+");
+                    default:
+                }
+                validation =0;
+
+                System.out.println("Digite uma senha novamente:");
+            }
+
         }
 
     }
